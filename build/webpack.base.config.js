f6f7b993c961b33bcf89d3b8a94a4ae65f468923
raylee1994 <849103991@qq.com>
2020-01-07 17:49:04 +0800

    2020-1-7 17:49

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,5 +1,19 @@
 var path = require("path");
 
 var entry = {
-    
-}
+    app: path.resolve(__dirname, "..", "src/main.js")
+};
+
+var module = {
+    rules: [
+        
+    ]
+};
+
+var plugins = [
+
+];
+
+var optimization = {
+
+};
