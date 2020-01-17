15182df09af990d1fbd0335daee030e62efded02
raylee1994 <849103991@qq.com>
2020-01-17 18:34:24 +0800

    2020-1-17 18:34

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,10 +1,11 @@
-const TerserPlugin = require("terser-webpack-plugin"); 
+const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const iswsl = require("is-wsl");
 const ora = require("ora");
 const chalk = require("chalk");
 const rimraf = require("rimraf");
 const path = require("path");
+const webpack = require("webpack");
 const config = require("./config");
 
 process.env.NODE_ENV = "production";
@@ -62,5 +63,27 @@ const spinner = ora("building for production");
 spinner.start();
 
 rimraf(path.resolve(__dirname, "..", config.outputPathname), err => {
+    if (err) throw err
+    webpack(webpackConfig, (errors, stats) => {
+        spinner.stop()
+        if (errors) throw errors
+        process.stdout.write(stats.toString({
+            colors: true,
+            modules: false,
+            children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
+            chunks: false,
+            chunkModules: false
+        }) + '\n\n')
 
+        if (stats.hasErrors()) {
+            console.log(chalk.red('  Build failed with errors.\n'))
+            process.exit(1)
+        }
+
+        console.log(chalk.cyan('  Build complete.\n'))
+        console.log(chalk.yellow(
+            '  Tip: built files are meant to be served over an HTTP server.\n' +
+            '  Opening index.html over file:// won\'t work.\n'
+        ))
+    })
 });
