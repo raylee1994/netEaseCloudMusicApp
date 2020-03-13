e13a129fd64bdf7f6fbbd40385626b599e100420
raylee1994 <849103991@qq.com>
2020-03-14 00:09:32 +0800

    2020-3-14

--- src/store/Player/action.js
+++ src/store/Player/action.js
@@ -5,9 +5,10 @@ export const switchPlayState = createAction(types.SWITCH_PLAY_STATE);
 export const editTrackQueue = createAction(types.EDIT_TRACK_QUEUE);
 export const editPlayerSetting = createAction(types.EDIT_PLAYER_SETTING);
 
-export const insertTrackQueue = function(params = {trackQueue: JSON.parse(localStorage.getItem("_player_track_queue")), song: null, triggerPlay: false}) {
+export const insertTrackQueue = function(params = {trackQueue: [], song: null, triggerPlay: false}) {
     return function(dispatch) {
         let {trackQueue, song, triggerPlay} = params
+        let playerSetting = JSON.parse(localStorage.getItem("_player_setting"))
         if(song) {
             let idArr = trackQueue.filter(item => {
                 return song.id == item.id
@@ -17,24 +18,59 @@ export const insertTrackQueue = function(params = {trackQueue: JSON.parse(localS
                 dispatch(editTrackQueue(trackQueue))
                 localStorage.setItem("_player_track_queue", JSON.stringify(trackQueue))
             }
-            triggerPlay && dispatch(editPlayerSetting({id: song.id}))
-            let playerSetting = JSON.parse(localStorage.getItem("_player_setting"))
             playerSetting = {
                 ...playerSetting,
-                id: song.id
+                index: playerSetting.index + 1
             }
-            localStorage.setItem("_player_setting", JSON.stringify(playerSetting))
         }else {
-            
+            let oldTrackQueue = triggerPlay ? [] : JSON.parse(localStorage.getItem("_player_track_queue"))
+            trackQueue = oldTrackQueue.concat(oldTrackQueue)
+            dispatch(editTrackQueue(trackQueue))
+            localStorage.setItem("_player_track_queue", JSON.stringify(trackQueue))
+            playerSetting = {
+                ...playerSetting,
+                index: 0
+            }
         }
+        triggerPlay && dispatch(editPlayerSetting({index: playerSetting.index}))
+        triggerPlay && localStorage.setItem("_player_setting", JSON.stringify(playerSetting))
     }
 }
 
-export const deleteSong = function(params = {trackQueue: JSON.parse(localStorage.getItem("_player_track_queue")), song: null}) {
+export const deleteSong = function(params = {trackQueue: [], song: null}) {
     return function(dispatch) {
         let {trackQueue, song} = params
         if(song) {
-            
+            let idArr = trackQueue.map(item => item.id)
+            let inArr = idArr.filter(item => song.id == item).length > 0
+            if(!inArr) {
+                return
+            }
+            let songIndex = idArr.indexOf(song.id)
+            let playerSetting = JSON.parse(localStorage.getItem("_player_setting"))
+            let currentIndex = playerSetting.index
+            let edit = false
+            if(songIndex < currentIndex) {
+                playerSetting = {
+                    ...playerSetting,
+                    index: currentIndex-1
+                }
+                edit = true
+            }
+            if(songIndex == currentIndex && currentIndex == trackQueue.length-1) {
+                playerSetting = {
+                    ...playerSetting,
+                    index: trackQueue.length-1
+                }
+                edit = true
+            }
+            trackQueue.splice(songIndex, 1)
+            dispatch(editTrackQueue(trackQueue))
+            localStorage.setItem("_player_track_queue", JSON.stringify(trackQueue))
+            if(edit) {
+                dispatch(editPlayerSetting({index: playerSetting.index}))
+                localStorage.setItem("_player_setting", JSON.stringify(playerSetting))
+            }
         }else {
             dispatch(editTrackQueue([]))
             localStorage.setItem("_player_track_queue", "[]")
--- src/store/Player/state.js
+++ src/store/Player/state.js
@@ -1,5 +1,5 @@
 export default {
     playState: false,
     trackQueue: JSON.parse(localStorage.getItem("_player_track_queue")) || [],
-    playerSetting: JSON.parse(localStorage.getItem("_player_setting")) || {mode: 4, volume: 0.8, autoPlay: false, id: null, lock: true}
+    playerSetting: JSON.parse(localStorage.getItem("_player_setting")) || {mode: 4, volume: 0.8, autoPlay: false, index: -1, lock: true}
 }
